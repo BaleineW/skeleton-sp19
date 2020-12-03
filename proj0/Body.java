3b1019e301b762237103e8fdeddbb7854ce8c36a
BaleineW <baleine.wang@gmail.com>
2020-12-03 16:06:41 -0500

    project 0 completed

---
+++ proj0/Body.java
@@ -0,0 +1,80 @@
+public class Body {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public Body(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Body(Body b){
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+
+    public double calcDistance(Body target){
+        double dx = xxPos - target.xxPos;
+        double dy = yyPos - target.yyPos;
+        double r = Math.hypot(dx, dy);
+        return r;
+    }
+
+    public double calcForceExertedBy(Body target){
+        double G = 6.67e-11;
+        double F = G * mass * target.mass / Math.pow(this.calcDistance(target), 2);
+        return F;
+    }
+
+    public double calcForceExertedByX(Body target){
+        double dx = Math.abs(xxPos - target.xxPos);
+        double Fx = this.calcForceExertedBy(target) * dx / this.calcDistance(target);
+        return Fx;
+    }
+
+    public double calcForceExertedByY(Body target){
+        double dy = Math.abs(yyPos - target.yyPos);
+        double Fy = this.calcForceExertedBy(target) * dy / this.calcDistance(target);
+        return Fy;
+    }
+
+    public double calcNetForceExertedByX(Body[] bodies){
+        double netFx = 0;
+        for (Body b : bodies){
+            if (!this.equals(b)) netFx += this.calcForceExertedByX(b);
+        }
+        return netFx;
+    }
+
+    public double calcNetForceExertedByY(Body[] bodies){
+        double netFy = 0;
+        for (Body b : bodies){
+            if (!this.equals(b)) netFy += this.calcForceExertedByY(b);
+        }
+        return netFy;
+    }
+
+    public void update(double dt, double Fx, double Fy){
+        double ax = Fx / this.mass;
+        double ay = Fy / this.mass;
+        this.xxVel += ax * dt;
+        this.yyVel += ay * dt;
+        this.xxPos += this.xxVel * dt;
+        this.yyPos += this.yyVel * dt;
+    }
+
+    public void draw() {
+        StdDraw.picture(this.xxPos, this.yyPos, "images/" + this.imgFileName);
+    }
+
+}
