24f6e486d2eb10d9e991585c77395ef1e5e7dc94
BaleineW <baleine.wang@gmail.com>
2020-12-15 14:29:06 -0500

    hw 2 completed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -20,7 +20,7 @@ public class Percolation {
     // create N-by-N grid, with all sited initially blocked
     public Percolation(int N) {
         if (N <= 0) {
-            throw new RuntimeException("The given size N is invalid!");
+            throw new IndexOutOfBoundsException("The given size N is invalid!");
         }
         grid = new boolean[N][N];
         size = N;
@@ -29,6 +29,11 @@ public class Percolation {
         bottom = N * N + 1;
         wuf = new WeightedQuickUnionUF(N * N + 2);
         wufNoBottom = new WeightedQuickUnionUF(N * N + 1);
+        for (int i = 0; i < size; i++){
+            wuf.union(top, toInt(0, i));
+            wufNoBottom.union(top, toInt(0, i));
+            wuf.union(bottom, toInt(size-1, i));
+        }
     }
     // transform 2d position to 1d
     private int toInt(int row, int col) {
@@ -37,7 +42,7 @@ public class Percolation {
     // validate
     private void validate(int row, int col) {
         if (row < 0 || row >= size || col < 0 || col >= size){
-            throw new RuntimeException("The input row/col is invaid!");
+            throw new IndexOutOfBoundsException("The input row/col is invaid!");
         }
     }
     // Open the site (row, col) if it is blocked
@@ -46,22 +51,21 @@ public class Percolation {
         if (! isOpen(row, col)) {
             grid[row][col] = true;
             numOpenSites += 1;
-        }
-        if (row == 0) {
-            wuf.union(top, toInt(row, col));
-            wufNoBottom.union(top, toInt(row, col));
-        } else if (row == size - 1) {
-            wuf.union(bottom, toInt(row, col));
-        }
-        for (int[] direction : directions) {
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-            if (0<=newRow && newRow<size && 0<=newCol && newCol<size && isOpen(newRow, newCol)) {
-                wuf.union(toInt(row, col), toInt(newRow, newCol));
-                wufNoBottom.union(toInt(row, col), toInt(newRow, newCol));
+            if (row == 0) {
+                wuf.union(top, toInt(row, col));
+                wufNoBottom.union(top, toInt(row, col));
+            } else if (row == size - 1) {
+                wuf.union(bottom, toInt(row, col));
+            }
+            for (int[] direction : directions) {
+                int newRow = row + direction[0];
+                int newCol = col + direction[1];
+                if (0 <= newRow && newRow < size && 0 <= newCol && newCol < size && isOpen(newRow, newCol)) {
+                    wuf.union(toInt(row, col), toInt(newRow, newCol));
+                    wufNoBottom.union(toInt(row, col), toInt(newRow, newCol));
+                }
             }
         }
-
     }
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
@@ -93,6 +97,10 @@ public class Percolation {
         pclt.open(2, 2);
         assertEquals(false, pclt.isFull(2, 2));
         assertTrue(pclt.isFull(1, 0));
+        Percolation pclt2 = new Percolation(1);
+        pclt2.open(0, 0);
+        assertEquals(true, pclt.percolates());
+        assertEquals(true, pclt.isFull(0, 0));
     }
 
 }
